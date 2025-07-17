25ec4c61b0d8e8074ef807dac5a789238dbed495
Ashmitha <ashmithavaisal37@gmail.com>
2025-07-17 18:36:33 +0530

    fix: apply SPA routing support for Vercel and Vite

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    historyApiFallback: true, // 🔁 important for dev server (optional but good)
+  },
+  build: {
+    outDir: 'dist', // default, but safe to be explicit
+  },
 })
