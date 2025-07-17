f9fdcda5b49cb02fd9a9ddfec5ee6e981ac8c6ca
Ashmitha <ashmithavaisal37@gmail.com>
2025-07-17 18:44:37 +0530

    deleted vercel.json and modified vite.config file

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    historyApiFallback: true, // 🔁 important for dev server (optional but good)
-  },
-  build: {
-    outDir: 'dist', // default, but safe to be explicit
-  },
+  plugins: [react(),tailwindcss()],
 })
